41f0f3cdc63e41b7b52760c3b48cb0788353b121
kakaxi <2284624967@qq.com>
2021-03-12 18:31:25 +0800

    3.12

---
+++ batchDocument/find.bat
@@ -0,0 +1,118 @@
+@echo off
+for /f "Delims= usebackq" %%i in ("C:\Users\ysb\Desktop\batchDocument\batchlearn\directory.txt") do (echo %%i)
+set /P label=请输入标签名：
+goto %LABEL%
+:@
+type "C:\Users\ysb\Desktop\batchDocument\batchlearn\1.txt">C:\Users\ysb\Desktop\batchDocument\记事本.txt
+exit
+:echo
+type "C:\Users\ysb\Desktop\batchDocument\batchlearn\2.txt">C:\Users\ysb\Desktop\batchDocument\记事本.txt
+exit
+:3
+type "C:\Users\ysb\Desktop\batchDocument\batchlearn\3.txt">C:\Users\ysb\Desktop\batchDocument\记事本.txt
+exit
+:pause
+type "C:\Users\ysb\Desktop\batchDocument\batchlearn\4.txt">C:\Users\ysb\Desktop\batchDocument\记事本.txt
+exit
+:goto
+type "C:\Users\ysb\Desktop\batchDocument\batchlearn\5.txt">C:\Users\ysb\Desktop\batchDocument\记事本.txt
+exit
+:%
+type "C:\Users\ysb\Desktop\batchDocument\batchlearn\6.txt">C:\Users\ysb\Desktop\batchDocument\记事本.txt
+exit
+:if
+type "C:\Users\ysb\Desktop\batchDocument\batchlearn\7.txt">C:\Users\ysb\Desktop\batchDocument\记事本.txt
+exit
+:call
+type "C:\Users\ysb\Desktop\batchDocument\batchlearn\8.txt">C:\Users\ysb\Desktop\batchDocument\记事本.txt
+exit
+:find
+type "C:\Users\ysb\Desktop\batchDocument\batchlearn\9.txt">C:\Users\ysb\Desktop\batchDocument\记事本.txt
+exit
+:set
+type "C:\Users\ysb\Desktop\batchDocument\batchlearn\10.txt">C:\Users\ysb\Desktop\batchDocument\记事本.txt
+exit
+:for
+type "C:\Users\ysb\Desktop\batchDocument\batchlearn\11.txt">C:\Users\ysb\Desktop\batchDocument\记事本.txt
+exit
+:12
+type "C:\Users\ysb\Desktop\batchDocument\batchlearn\12.txt">C:\Users\ysb\Desktop\batchDocument\记事本.txt
+exit
+:13
+type "C:\Users\ysb\Desktop\batchDocument\batchlearn\13.txt">C:\Users\ysb\Desktop\batchDocument\记事本.txt
+exit
+:14
+type "C:\Users\ysb\Desktop\batchDocument\batchlearn\14.txt">C:\Users\ysb\Desktop\batchDocument\记事本.txt
+exit
+:15
+type "C:\Users\ysb\Desktop\batchDocument\batchlearn\15.txt">C:\Users\ysb\Desktop\batchDocument\记事本.txt
+exit
+:16
+type "C:\Users\ysb\Desktop\batchDocument\batchlearn\16.txt">C:\Users\ysb\Desktop\batchDocument\记事本.txt
+exit
+:17
+type "C:\Users\ysb\Desktop\batchDocument\batchlearn\17.txt">C:\Users\ysb\Desktop\batchDocument\记事本.txt
+exit
+:18
+type "C:\Users\ysb\Desktop\batchDocument\batchlearn\18.txt">C:\Users\ysb\Desktop\batchDocument\记事本.txt
+exit
+:19
+type "C:\Users\ysb\Desktop\batchDocument\batchlearn\19.txt">C:\Users\ysb\Desktop\batchDocument\记事本.txt
+exit
+:start
+type "C:\Users\ysb\Desktop\batchDocument\batchlearn\20.txt">C:\Users\ysb\Desktop\batchDocument\记事本.txt
+exit
+:type
+type "C:\Users\ysb\Desktop\batchDocument\batchlearn\21.txt">C:\Users\ysb\Desktop\batchDocument\记事本.txt
+exit
+:22
+type "C:\Users\ysb\Desktop\batchDocument\batchlearn\22.txt">C:\Users\ysb\Desktop\batchDocument\记事本.txt
+exit
+:23
+type "C:\Users\ysb\Desktop\batchDocument\batchlearn\23.txt">C:\Users\ysb\Desktop\batchDocument\记事本.txt
+exit
+:md
+type "C:\Users\ysb\Desktop\batchDocument\batchlearn\24.txt">C:\Users\ysb\Desktop\batchDocument\记事本.txt
+exit
+:rd
+type "C:\Users\ysb\Desktop\batchDocument\batchlearn\25.txt">C:\Users\ysb\Desktop\batchDocument\记事本.txt
+exit
+:ren
+type "C:\Users\ysb\Desktop\batchDocument\batchlearn\26.txt">C:\Users\ysb\Desktop\batchDocument\记事本.txt
+exit
+:move
+type "C:\Users\ysb\Desktop\batchDocument\batchlearn\27.txt">C:\Users\ysb\Desktop\batchDocument\记事本.txt
+exit
+:28
+type "C:\Users\ysb\Desktop\batchDocument\batchlearn\28.txt">C:\Users\ysb\Desktop\batchDocument\记事本.txt
+exit
+:29
+type "C:\Users\ysb\Desktop\batchDocument\batchlearn\29.txt">C:\Users\ysb\Desktop\batchDocument\记事本.txt
+exit
+:30
+type "C:\Users\ysb\Desktop\batchDocument\batchlearn\30.txt">C:\Users\ysb\Desktop\batchDocument\记事本.txt
+exit
+:attrib
+type "C:\Users\ysb\Desktop\batchDocument\batchlearn\31.txt">C:\Users\ysb\Desktop\batchDocument\记事本.txt
+exit
+:del
+type "C:\Users\ysb\Desktop\batchDocument\batchlearn\32.txt">C:\Users\ysb\Desktop\batchDocument\记事本.txt
+exit
+:copy
+type "C:\Users\ysb\Desktop\batchDocument\batchlearn\33.txt">C:\Users\ysb\Desktop\batchDocument\记事本.txt
+exit
+:xcopy
+type "C:\Users\ysb\Desktop\batchDocument\batchlearn\34.txt">C:\Users\ysb\Desktop\batchDocument\记事本.txt
+exit
+:choice
+type "C:\Users\ysb\Desktop\batchDocument\batchlearn\35.txt">C:\Users\ysb\Desktop\batchDocument\记事本.txt
+exit
+:cd
+type "C:\Users\ysb\Desktop\batchDocument\batchlearn\36.txt">C:\Users\ysb\Desktop\batchDocument\记事本.txt
+exit
+:dir
+type "C:\Users\ysb\Desktop\batchDocument\batchlearn\37.txt">C:\Users\ysb\Desktop\batchDocument\记事本.txt
+exit
+:shift
+type "C:\Users\ysb\Desktop\batchDocument\batchlearn\38.txt">C:\Users\ysb\Desktop\batchDocument\记事本.txt
+exit
